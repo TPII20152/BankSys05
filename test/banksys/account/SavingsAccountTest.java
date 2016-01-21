d5b5e9eff75e06b31d5acfad7e1adbc084a78b59
Thiago <thiagoisaiasbrg@gmail.com>
2016-01-21 01:16:22 -0300

    gitignore updated / SavingsAccountTest done #5

--- test/banksys/account/SavingsAccountTest.java
+++ test/banksys/account/SavingsAccountTest.java
@@ -11,9 +11,11 @@ import static org.junit.Assert.*;
  */
 public class SavingsAccountTest {
 
+    SavingsAccount sAccount;
+
     @Before
     public void setUp() throws Exception {
-
+        sAccount = new SavingsAccount("918543");
     }
 
     @After
@@ -23,26 +25,45 @@ public class SavingsAccountTest {
 
     @Test
     public void testEarnInterest() throws Exception {
-
+        double value = 100;
+        sAccount.credit(value);
+        double oldBalance = sAccount.getBalance();
+        sAccount.earnInterest();
+        double newBalance = sAccount.getBalance();
+        assertEquals(newBalance,oldBalance*1.001,0);
     }
 
     @Test
     public void testCredit() throws Exception {
-
+        double value = 100;
+        double oldBalance = sAccount.getBalance();
+        sAccount.credit(value);
+        double newBalance = sAccount.getBalance();
+        assertEquals(value,newBalance-oldBalance,0);
     }
 
     @Test
     public void testDebit() throws Exception {
-
+        double valueCredit = 100;
+        sAccount.credit(valueCredit);
+        double oldBalance = sAccount.getBalance();
+        double valueDebit = 50;
+        sAccount.debit(50);
+        double newBalance = sAccount.getBalance();
+        assertEquals(valueDebit,oldBalance-newBalance,0);
     }
 
     @Test
     public void testGetNumber() throws Exception {
-
+        assertEquals("918543",sAccount.getNumber());
     }
 
     @Test
     public void testGetBalance() throws Exception {
-
+        double oldBalance = sAccount.getBalance();
+        double value = 10;
+        sAccount.credit(value);
+        double newBalance = sAccount.getBalance();
+        assertEquals(value,newBalance-oldBalance,0);
     }
 }
