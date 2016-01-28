fed17f0c2ce56d54a00e057070c85d9ee1597e48
Thiago <thiagoisaiasbrg@gmail.com>
2016-01-28 18:26:33 -0300

    closes #3

--- src/banksys/logging/Logger.java
+++ src/banksys/logging/Logger.java
@@ -1,9 +1,10 @@
 package banksys.logging;
 
 
-import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by thiagoisaias on 1/25/16.
@@ -12,15 +13,20 @@ public class Logger {
 
     public static void write(String message){
 
-        String logFile = "logFile.txt";
-        String time = LocalDateTime.now().toString();
+        System.out.println("Chamou a Logger.write");
 
         try{
-            FileWriter fileWriter = new FileWriter(logFile);
-            BufferedWriter bfWriter = new BufferedWriter(fileWriter);
+            String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
 
-            bfWriter.write("["+ time + "] - " + message);
-            bfWriter.close();
+            File logFile = new File("log/logFile.txt");
+            if (!logFile.exists()){
+                logFile.createNewFile();
+            }
+
+            FileWriter fileWriter = new FileWriter(logFile.getAbsoluteFile(),true);
+            fileWriter.write("["+ time + "] - " + message + "\n");
+            fileWriter.close();
+            System.out.println(time);
 
         }catch (Exception e) {
             e.printStackTrace();
